71cbf6673da11ecd8822671262329923865b151c
Kanika Saini <kanika16047@iiitd.ac.in>
2017-11-15 17:10:17 +0530

    commited

---
+++ src/controllers/AvailableRooms_AdminController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AvailableRooms_AdminController {
+	@FXML private TextField roomNo;
+	@FXML protected void handleCheckButtonAction(ActionEvent event) throws Exception {
+		 	String roomNumber = roomNo.getText();
+
+	 }
+}
--- src/controllers/User_AdminController.java
+++ src/controllers/User_AdminController.java
@@ -1,5 +1,37 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class User_AdminController {
 
+	@FXML private Menu availableRooms;
+	@FXML private Text username;
+	 @FXML protected void handleAvailableRoomsButtonAction(ActionEvent event) throws Exception {
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/AvailableRooms_Admin.fxml"));
+			Scene homepage = new Scene(rootHomepage);
+			((Stage)username.getScene().getWindow()).setScene(homepage);
+	 }
+	 @FXML protected void handleBookRoomsButtonAction(ActionEvent event) throws Exception {
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/BookRoom_Admin.fxml"));
+			Scene homepage = new Scene(rootHomepage);
+			((Stage)username.getScene().getWindow()).setScene(homepage);
+	 }
+	 @FXML protected void handleMyBookingsButtonAction(ActionEvent event) throws Exception {
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/MyBookings_Admin.fxml"));
+			Scene homepage = new Scene(rootHomepage);
+			((Stage)username.getScene().getWindow()).setScene(homepage);
+	 }
+	 @FXML protected void handleHandleRequestsButtonAction(ActionEvent event) throws Exception {
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/HandleRequests_Admin.fxml"));
+			Scene homepage = new Scene(rootHomepage);
+			((Stage)username.getScene().getWindow()).setScene(homepage);
+	 }
 }
