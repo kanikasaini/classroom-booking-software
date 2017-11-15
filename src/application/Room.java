58a0244f9cea26d8dcaa5e88808f699894591b11
Kanika Saini <kanika16047@iiitd.ac.in>
2017-11-16 01:20:54 +0530

    conflict expected

--- src/application/Room.java
+++ src/application/Room.java
@@ -1,9 +1,16 @@
 package application;
+import java.io.Serializable;
 import java.util.*;
 
-public class Room {
+public class Room implements Serializable {
 
 private String number;
 private int capacity;
 private ArrayList<String> availableSlots;
+public Room(String number, int capacity, ArrayList<String> availableSlots) {
+	super();
+	this.number = number;
+	this.capacity = capacity;
+	this.availableSlots = availableSlots;
+}
 }
