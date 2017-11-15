16455cb2042b7d42628bf03d3b7fdb9a217897cb
Isha Kalra <isha16152@iiitd.ac.in>
2017-11-16 00:59:33 +0530

    menu connection done

--- src/controllers/BookRoom_FacultyController.java
+++ src/controllers/BookRoom_FacultyController.java
@@ -6,34 +6,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class AvailableRooms_AdminController {
+public class BookRoom_FacultyController {
 
 	@FXML private MenuBar mainNavBar;
 	@FXML private Button checkBtn;
 	
 	 @FXML protected void handleHomeButton(ActionEvent event) throws Exception {
-		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/User_Admin.fxml"));
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/User_Faculty.fxml"));
 			Scene homepage = new Scene(rootHomepage);
 			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
 	 }
-	 @FXML protected void handleBookRoomButton(ActionEvent event) throws Exception {
-		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/BookRoom_Admin.fxml"));
+	 @FXML protected void handleAvailableRoomsButton(ActionEvent event) throws Exception {
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/AvailableRooms_Faculty.fxml"));
 			Scene homepage = new Scene(rootHomepage);
 			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
 	 }
 	 @FXML protected void handleMyBookingsButton(ActionEvent event) throws Exception {
-		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/MyBookings_Admin.fxml"));
-			Scene homepage = new Scene(rootHomepage);
-			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
-	 }
-	 @FXML protected void handleHandleRequestsButton(ActionEvent event) throws Exception {
-		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/HandleRequests_Admin.fxml"));
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/MyBookings_Faculty.fxml"));
 			Scene homepage = new Scene(rootHomepage);
 			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
 	 }
--- src/controllers/HandleRequests_AdminController.java
+++ src/controllers/HandleRequests_AdminController.java
@@ -6,35 +6,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class AvailableRooms_AdminController {
+public class HandleRequests_AdminController {
 
 	@FXML private MenuBar mainNavBar;
-	@FXML private Button checkBtn;
+	@FXML private Label handleRequestsLabel;
 	
 	 @FXML protected void handleHomeButton(ActionEvent event) throws Exception {
 		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/User_Admin.fxml"));
 			Scene homepage = new Scene(rootHomepage);
-			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
+			((Stage)handleRequestsLabel.getScene().getWindow()).setScene(homepage);
 	 }
-	 @FXML protected void handleBookRoomButton(ActionEvent event) throws Exception {
-		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/BookRoom_Admin.fxml"));
+	 @FXML protected void handleAvailableRoomsButton(ActionEvent event) throws Exception {
+		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/AvailableRooms_Admin.fxml"));
 			Scene homepage = new Scene(rootHomepage);
-			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
+			((Stage)handleRequestsLabel.getScene().getWindow()).setScene(homepage);
+	 }
+			
+	 @FXML protected void handleBookRoomButton(ActionEvent event) throws Exception {
+			Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/BookRoom_Admin.fxml"));
+			Scene homepage = new Scene(rootHomepage);
+			((Stage)handleRequestsLabel.getScene().getWindow()).setScene(homepage);			
 	 }
 	 @FXML protected void handleMyBookingsButton(ActionEvent event) throws Exception {
 		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/MyBookings_Admin.fxml"));
 			Scene homepage = new Scene(rootHomepage);
-			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
-	 }
-	 @FXML protected void handleHandleRequestsButton(ActionEvent event) throws Exception {
-		 	Parent rootHomepage = FXMLLoader.load(getClass().getResource("../view/HandleRequests_Admin.fxml"));
-			Scene homepage = new Scene(rootHomepage);
-			((Stage)checkBtn.getScene().getWindow()).setScene(homepage);
+			((Stage)handleRequestsLabel.getScene().getWindow()).setScene(homepage);
 	 }
 }
+
