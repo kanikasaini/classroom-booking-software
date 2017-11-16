9271854bad98c8d65b2a895289093cda1df0b5fc
Kanika Saini <kanika16047@iiitd.ac.in>
2017-11-16 12:23:40 +0530

    room class updated

--- src/application/Room.java
+++ src/application/Room.java
@@ -4,13 +4,97 @@ import java.util.*;
 
 public class Room implements Serializable {
 
-private String number;
-private int capacity;
-private ArrayList<String> availableSlots;
-public Room(String number, int capacity, ArrayList<String> availableSlots) {
-	super();
-	this.number = number;
-	this.capacity = capacity;
-	this.availableSlots = availableSlots;
-}
+public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public ArrayList<String> getbookedSlots() {
+		return bookedSlots;
+	}
+	public void setbookedSlots(ArrayList<String> bookedSlots) {
+		this.bookedSlots = bookedSlots;
+	}
+	public void addSlot(String slot)
+	{
+		this.bookedSlots.add(slot);
+	}
+	public String number;
+    private int capacity;
+    public ArrayList<String> bookedSlots = new ArrayList<String>();
+	public Room(String number, int capacity, ArrayList<String> bookedSlots) {
+		super();
+		this.number = number;
+		this.capacity = capacity;
+		this.bookedSlots = bookedSlots;
+	}
+	public Room(String number, int capacity) {
+		super();
+		this.number = number;
+		this.capacity = capacity;
+	}
+	public boolean checkOverlap(String day, String start1, String end1)
+	{
+		boolean flag= false;
+		for(int i=0; i<bookedSlots.size(); i++)
+		{
+			String slot= bookedSlots.get(i);
+			String[] dayntime = slot.split(" ");
+			String[] timeS2E = dayntime[1].split("-");
+			String start2=timeS2E[0]; String end2=timeS2E[1];
+			if(dayntime[0].equalsIgnoreCase(day))
+			{
+				if( (greater(end1,start2)==1 & greater(start2,start1)!=0) | (greater(end1,end2)!=0 & greater(end2,start1)==1) )
+					flag=true;
+				else
+					flag=false;
+				System.out.println(flag);
+			}
+		}
+		return flag;
+	}
+	public int greater(String start, String end) //returns 1 if start>end, returns 0 if start<=end
+	{
+		String[] start1 = start.split("");
+		String[] end1 = end.split("");
+		int time1=0; int time2=0; int start_is=0; int end_is=0;
+		if(start1.length==4)
+		{
+			time1=Integer.parseInt(start1[0]);
+			start_is=(time1-8)%12;
+		}
+		else
+			start_is=(Integer.parseInt(start1[0].concat(start1[1]))-8)%12;
+		if(end1.length==4)
+		{
+			time2=Integer.parseInt(end1[0]);
+			end_is=(time2-8)%12;
+		}
+		else
+			end_is=(Integer.parseInt(end1[0].concat(end1[1]))-8)%12;
+		if(start_is<0)
+			start_is+=12;
+		if(end_is<0)
+			end_is+=12;
+		if(start_is<end_is)
+			return 0;
+		else if(start_is>end_is)
+			return 1;
+		else
+		{
+			if(Integer.parseInt(start1[start1.length-2]) > Integer.parseInt(end1[end1.length-2]))
+				return 1;
+			else if(Integer.parseInt(start1[start1.length-2]) < Integer.parseInt(end1[end1.length-2]))
+				return 0;
+			else
+				return -1;
+		}
+	}
 }
